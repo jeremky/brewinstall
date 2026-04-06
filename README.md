4ed42532711cd2c46f8a94ee15967c157d396093
JeremKy <jeremky@me.com>
2026-04-06 14:23:31 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# brewinstall.sh
+
+Ce script a pour objectif d'installer [brew](https://brew.sh/) ainsi que des applications cli et MacOS présentes dans des fichiers dédiés.
+
+Le script va également effectuer les opérations suivantes :
+
+- désactiver la création des fichiers `.DS_Store` sur les partages réseau.
+- activer la commande `locate`
+
+## Préparation
+
+Avant d'exécuter le script, il est nécessaire de vérifier les fichiers suivants :
+
+- `brewinstall.apps.cfg`
+- `brewinstall.cask.cfg`
+
+Ces fichiers contiennent les applications qui seront automatiquement installées.
+Les lignes commençant par `#` sont ignorées.
+
+## Utilisation
+
+1. Clonez ou téléchargez ce répertoire
+2. Éditez `brewinstall.apps.cfg` et `brewinstall.cask.cfg` selon vos besoins
+3. Exécutez le script :
+
+```bash
+cd brewinstall
+./brewinstall.sh
+```
+
+> Le script demandera votre mot de passe sudo pour l'opération concernant les fichiers `.DS_Store` et l'activation de la commande `locate`
---
+++ brewinstall.sh
@@ -0,0 +1,35 @@
+#!/bin/zsh -e
+
+dir=$(dirname "$0")
+
+# Désactivation des .DS_Store sur le réseau
+if [[ $(defaults read /Library/Preferences/com.apple.desktopservices DSDontWriteNetworkStores) = 0 ]]; then
+  sudo defaults write /Library/Preferences/com.apple.desktopservices DSDontWriteNetworkStores -bool true
+fi
+
+# Activation de la commande locate
+if [[ $(sudo launchctl list | grep -c "com.apple.locate") -eq 0 ]]; then
+  sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.locate.plist
+fi
+
+# Déterminer le chemin de Homebrew selon l'architecture
+if [[ $(uname -m) == "arm64" ]]; then
+  BREW_PATH="/opt/homebrew/bin/brew"
+else
+  BREW_PATH="/usr/local/bin/brew"
+fi
+
+# Installation de Brew
+if [[ ! -f $BREW_PATH ]]; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  grep -q 'brew shellenv' "$HOME/.zprofile" || echo "eval \"\$($BREW_PATH shellenv)\"" >> "$HOME/.zprofile"
+  eval "$($BREW_PATH shellenv)"
+else
+  echo "Brew est déjà installé"
+fi
+
+# Installation des applications cli
+brew install $(grep -v '#' "$dir/brewinstall.apps.cfg")
+
+# Installation des applications MacOS
+brew install --cask $(grep -v '#' "$dir/brewinstall.cask.cfg")
